3f86b29de591102de5beb93035e65c623c556332
Ryan Sutrisno <ryan.sutrisno@gmail.com>
2023-10-03 21:06:03 +0700

    get single document event by id

--- src/lib/events.ts
+++ src/lib/events.ts
@@ -1,4 +1,5 @@
-import { databases } from "./appwrite";
+import { Models } from "appwrite";
+import { databases } from "@/lib/appwrite";
 import { LiveBeatEvent } from "@/types/events";
 
 export async function getEvents() {
@@ -7,14 +8,27 @@ export async function getEvents() {
     import.meta.env.VITE_APPWRITE_EVENTS_COLLECTIONS_ID
   );
   return {
-    events: documents.map((document) => {
-      const event: LiveBeatEvent = {
-        $id: document.$id,
-        name: document.name,
-        location: document.location,
-        date: document.date,
-      };
-      return event;
-    }),
+    events: documents.map(mapDocumentToEvent),
   };
+}
+
+export async function getEventById(eventId: string) {
+  const document = await databases.getDocument(
+    import.meta.env.VITE_APPWRITE_EVENTS_DATABASES_ID,
+    import.meta.env.VITE_APPWRITE_EVENTS_COLLECTIONS_ID,
+    eventId
+  );
+  return {
+    event: mapDocumentToEvent(document),
+  };
+}
+
+function mapDocumentToEvent(document: Models.Document) {
+  const event: LiveBeatEvent = {
+    $id: document.$id,
+    name: document.name,
+    location: document.location,
+    date: document.date,
+  };
+  return event;
 }
--- src/pages/event/[eventId].tsx
+++ src/pages/event/[eventId].tsx
@@ -1,21 +1,31 @@
-import Layout from '@/components/Layout';
-import Container from '@/components/Container';
+import { useEffect, useState } from "react";
+import Layout from "@/components/Layout";
+import Container from "@/components/Container";
 // import Button from '@/components/Button';
 
-import events from '@/data/events.json';
+import { getEventById } from "@/lib/events";
+import { LiveBeatEvent } from "@/types/events";
 
-function Event() {
-  const event = events[0];
-  const image = {
-    url: events[0].imageUrl,
-    alt: ''
-  };
+function Event({ params }: { params: { eventId: string } }) {
+  // const image = {
+  //   url: events[0].imageUrl,
+  //   alt: "",
+  // };
+
+  const [event, setEvent] = useState<LiveBeatEvent | undefined>();
+
+  useEffect(() => {
+    (async function run() {
+      const { event } = await getEventById(params.eventId);
+      setEvent(event);
+    })();
+  }, [params.eventId]);
 
   return (
     <Layout>
       <Container className="grid gap-12 grid-cols-1 md:grid-cols-2">
         <div>
-          {image?.url && (
+          {/* {image?.url && (
             <img
               className="block rounded"
               width={800}
@@ -23,20 +33,23 @@ function Event() {
               src={image.url}
               alt={image.alt}
             />
-          )}
+          )} */}
         </div>
 
         <div>
           {event && (
             <>
-              <h1 className="text-3xl font-bold mb-6">
-                { event?.name }
-              </h1>
+              <h1 className="text-3xl font-bold mb-6">{event?.name}</h1>
               <p className="text-lg font-medium text-neutral-600 dark:text-neutral-200">
-                <strong>Date:</strong> { event?.date && new Date(event?.date).toLocaleString('en-US', { month: 'long', day: 'numeric' }) }
+                <strong>Date:</strong>{" "}
+                {event?.date &&
+                  new Date(event?.date).toLocaleString("en-US", {
+                    month: "long",
+                    day: "numeric",
+                  })}
               </p>
               <p className="text-lg font-medium text-neutral-600 dark:text-neutral-200">
-                <strong>Location:</strong> { event?.location }
+                <strong>Location:</strong> {event?.location}
               </p>
               {/* <p className="mt-6">
                 <Button color="red">Delete Event</Button>
@@ -46,7 +59,7 @@ function Event() {
         </div>
       </Container>
     </Layout>
-  )
+  );
 }
 
 export default Event;
